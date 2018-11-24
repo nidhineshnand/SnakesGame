6dc07fb678970147e0f16ff0fa1ea0da0212944a
nidhineshnand <nnan773@aucklanduni.ac.nz>
2018-11-25 04:38:26 +1300

    Added a method that randomly generates a number when it is called for the food

--- src/Food.java
+++ src/Food.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+
 public class Food {
     private int _xCoordinate;
     private int _yCoordinate;
-    private int boxSize;
+    private int _boxSize;
+
+    Food(int boxSize){
+        _boxSize = boxSize;
+    }
+
+    //Method gets random coordinate for the food
+    public int getFoodCoor(){
+        Random rand = new Random();
+        return rand.nextInt(_boxSize);
+    }
 }
